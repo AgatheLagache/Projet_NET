6672866e40d0c7465cdbc817651c501d1d81b780
AgatheLagache <agathe.lagache@viacesi.fr>
2018-12-12 13:10:51 +0100

    Création des projets vierges

---
+++ NET_Project/Console_NET_Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_NET_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
