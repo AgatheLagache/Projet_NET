adcb953ae1bf8245e8db682c9be33c71c0a52b69
Kévin BUNDHUN <kevin.bundhun@viacesi.fr>
2018-12-12 14:53:12 +0100

    implementation of socket server

---
+++ NET_Project/Console_NET_Project/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_NET_Project
+{
+    public class Client
+    {
+        private TcpClient client;
+        public Client(TcpClient client)
+        {
+            this.client = client;
+        }
+        
+        public void Communication()
+        {
+            NetworkStream stream = client.GetStream();
+            client.Close();
+        }
+    }
+}
---
+++ NET_Project/Console_NET_Project/SocketServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Console_NET_Project
+{
+    public class SocketServer
+    {
+        static void Main(string[] args)
+        {
+            int serverPort = 8888;
+            IPAddress serverAddress = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = null;
+
+            try
+            {
+                serverSocket = new TcpListener(serverAddress, serverPort);
+
+                // Start the socket
+                serverSocket.Start();
+                Console.WriteLine("Connection au serveur réussi");
+
+                while (true)
+                {
+                    TcpClient client = serverSocket.AcceptTcpClient();
+
+                    Client user = new Client(client);
+
+                    Thread thread = new Thread(() => user.Communication());
+                    thread.Start();
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Exception : {0}", error);
+            }
+            finally
+            {
+                serverSocket.Stop();
+            }
+        }
+    }
+}
