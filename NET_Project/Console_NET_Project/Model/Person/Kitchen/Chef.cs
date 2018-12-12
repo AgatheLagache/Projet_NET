f743b1ff5e9c9df8b17f5cd885c286c74d5a8301
Kévin BUNDHUN <kevin.bundhun@viacesi.fr>
2018-12-12 22:05:10 +0100

    Add class copied

---
+++ NET_Project/Console_NET_Project/Model/Person/Kitchen/Chef.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_NET_Project.Model.Person.Kitchen
+{
+    public class Chef
+    {
+        public Chef()
+        {
+
+        }
+        public void TakeCommand()
+        {
+            /* prend la commande donnée par le chef de rang */
+        }
+
+        public void DispatchTask()
+        {
+            /* réparti les différentes tâches à réaliser aux commis chef */
+        }
+
+        public void SortOrder()
+        {
+            /* trie les commandes afin d'optimiser la production des commandes */
+        }
+    }
+}
