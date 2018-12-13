02004f18170d5a570af72fb3939f45acaa45ec33
LucasLartot <lucas.lartotdaluzrijo@viacesi.fr>
2018-12-13 08:54:58 +0100

    Add code

--- NET_Project/WPF_NET_Project/MainWindow.xaml.cs
+++ NET_Project/WPF_NET_Project/MainWindow.xaml.cs
@@ -24,5 +24,72 @@ namespace WPF_NET_Project
         {
             InitializeComponent();
         }
+
+
+        ///* déclaration du model restaurant de l'interface IRestaurantModel */
+        //private IRestaurantModel restaurantModel;
+
+        ///* déclaration de la vue restaurant de l'interface IRestaurantView */
+        //private IRestaurantView restaurantView;
+        //private IScenario scenario;
+
+        ///* Instancie un nouveau controleur du restaurant */
+        //public RestaurantController(IRestaurantModel restaurantModel, IRestaurantView restaurantView)
+        //{
+        //    this.setRestaurantView(restaurantView);
+        //    this.setRestaurantModel(restaurantModel);
+        //}
+
+        ///* Récupère le model du restaurant et le retourne */
+        //public IRestaurantModel getRestaurantModel()
+        //{
+        //    return this.restaurantModel;
+        //}
+
+        ///* Attribue la valeur du model */
+        //private void setRestaurantModel(IRestaurantModel model)
+        //{
+        //    this.restaurantModel = model;
+        //}
+
+        ///* Récupère la vue du restaurant et la retourne */
+        //public IRestaurantView getRestaurantView()
+        //{
+        //    return this.restaurantView;
+        //}
+
+        ///* Attribue la valeur de la vue */
+        //private void setRestaurantView(IRestaurantView view)
+        //{
+        //    this.restaurantView = view;
+        //}
+
+        ///* Lance la méthode initializeScenario qui permet de choisir un scnario */
+        //public void startScenario()
+        //{
+        //    scenario.initializeScenario(1);
+        //}
+
+        ///*  */
+        //public void play()
+        //{
+        //    this.startScenario();
+        //}
+
+        ///*  */
+        //public void pause()
+        //{
+
+        //}
+
+        ///*  */
+        //public void replay()
+        //{
+
+        //}
+
+        //public void speedUp()
+        //{
+        //}
     }
 }
