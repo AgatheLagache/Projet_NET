a774904fdc13330482f69b2461e24a6062480204
LucasLartot <lucas.lartotdaluzrijo@viacesi.fr>
2018-12-13 10:14:22 +0100

    Add parametres.cs

--- NET_Project/WPF_NET_Project/MainWindow.xaml.cs
+++ NET_Project/WPF_NET_Project/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_NET_Project
 {
@@ -33,6 +20,8 @@ namespace WPF_NET_Project
         int Nb_Slow_Clients = 1;
         int Nb_Waiters = 1;
         int Nb_Cooks = 1;
+
+
 
         public MainWindow()
         {
